0f2a2fe0f7f5ff7203769242c1b4955cfc896e62
diegodasp88 <diego.gps@gmail.com>
2025-08-06 21:28:10 -0300

    atualizei o App com variáveis e chamando os cards e a navegação

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Card } from "./Card"
+import { NavBar } from './NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const questions = [
+    {text: "O que é JSX?", answer: "Uma extensão de linguagem JavaScript."},
+    {text: "O que é React?", answer: "É uma biblioteca JavaScript para construção de interface gráfica."},
+    {text: "O que é um componente?", answer: "Uma função ou classe que retorna um elemento React."}
+  ]
+
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [answerIsVisible, setAnswerIsVisible] = useState(false)
+
+  const currentQuestion = questions[currentIndex]
+
+  const lastIndex = questions.length - 1
 
   return (
     <>
-      <Card/>
+      <Card text={answerIsVisible ? currentQuestion.answer : currentQuestion.text}/>
+      <NavBar 
+        setCurrentIndex={setCurrentIndex} 
+        currentIndex={currentIndex} 
+        lastIndex={lastIndex}
+        answerIsVisible={answerIsVisible}
+        setAnswerIsVisible={setAnswerIsVisible} />
     </>
   )
 }
